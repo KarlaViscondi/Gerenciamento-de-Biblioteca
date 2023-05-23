516eb5cb447c3725cfe0aceb89bce1fe2814e35c
KarlaViscondi <90910654+KarlaViscondi@users.noreply.github.com>
2023-05-23 22:02:09 +0000

    Components

--- gerenciamento-biblioteca/src/components/common/Footerbar.tsx
+++ gerenciamento-biblioteca/src/components/common/Footerbar.tsx
@@ -5,11 +5,11 @@ import {FaLinkedin} from 'react-icons/fa'
 const Footerbar = () => {
     return (
         <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-            <div className="items-center grid-flow-col">
-                <BiBook className="h-6 w-6"/>
+            <div className="items-center grid-flow-col text-center">
+                <BiBook className="h-6 w-6 items-center text-center"/>
                 <p>Copyright © 2023 - Todos os direitos reservados</p>
             </div> 
-            <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end items-center text-center">
                 <FaGithub className="h-6 w-6"/>
                 <FaLinkedin className="h-6 w-6"/>
             </div>
--- gerenciamento-biblioteca/src/pages/index.tsx
+++ gerenciamento-biblioteca/src/pages/index.tsx
@@ -9,32 +9,54 @@ import Modal from '../components/common/Modal'
 
 export default function Home() {
   return (
-    <main className='bg-[#201b2c] h-max'>
-      <Navbar></Navbar>
-      <div className='flex flex-row mx-2 justify-center items-center'>
-        <div >
-          <Searchbox></Searchbox>
-          <div className='flex mt-12'>
-            {/* Modal 1 */}
-            <Modal className="modal-button">
-              <h3>Livros selecionados</h3>
-              <div className='flex flex-col mb-2'>
+    <div className="flex flex-col min-h-screen overflow-y-hidden">
+      <header className="bg-[#201b2c]">
+        <Navbar />
+      </header>
+      <main className='flex-grow bg-[#201b2c] overflow-y-auto '>
+        <div className='flex flex-col h-full mx-2 justify-center items-center'>
+          <div >
+            <Searchbox></Searchbox>
+            <div className="border border-gray-300 rounded-lg flex flex-col p-4 mt-6">
+              {/* Conteúdo dos livros */}
+              <div className='text-white'>
+                <Checkbox> </Checkbox>
+                <Checkbox> </Checkbox>
+                <Checkbox> </Checkbox>
+                <Checkbox> </Checkbox>
+                <Checkbox> </Checkbox>
+                <Checkbox> </Checkbox>
                 <Checkbox> </Checkbox>
                 <Checkbox> </Checkbox>
                 <Checkbox> </Checkbox>
                 <Checkbox> </Checkbox>
               </div>
-            </Modal>
-            <Button children={"Cancelar"} className='bg-red-500 rounded-lg	w-4/5 text-center uppercase tracking-wider font-extrabold mh-6 p-3 mb-6' />
+            </div>
+
+            <div className='flex mt-12'>
+              {/* Modal 1 */}
+              <Modal className="modal-button">
+                <h3>Livros selecionados</h3>
+                <div className='flex flex-col mb-2'>
+                  <Checkbox> </Checkbox>
+                  <Checkbox> </Checkbox>
+                  <Checkbox> </Checkbox>
+                  <Checkbox> </Checkbox>
+                </div>
+              </Modal>
+              <Button children={"Cancelar"} className='bg-red-500 rounded-lg	w-4/5 text-center uppercase tracking-wider font-extrabold mh-6 p-3 mb-6' />
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            {/* <Checkbox> </Checkbox>
+            <Checkbox> </Checkbox>
+            <Checkbox> </Checkbox> */}
           </div>
         </div>
-        <div className='flex flex-col'>
-          {/* <Checkbox> </Checkbox>
-          <Checkbox> </Checkbox>
-          <Checkbox> </Checkbox> */}
-        </div>
-      </div>
-      <Footerbar></Footerbar>
-    </main>
+      </main>
+      <footer className="bg-[#201b2c] text-center items-center">
+        <Footerbar />
+      </footer>
+    </div>
   )
 }

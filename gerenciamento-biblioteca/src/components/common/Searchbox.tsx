c2a44fc2c4fa211d701254a2c5813946e9dc401b
Thabata Moraes <thabatams16@gmail.com>
2023-06-08 16:40:30 -0300

    Feat: Change Searchbox component

--- gerenciamento-biblioteca/src/components/common/Searchbox.tsx
+++ gerenciamento-biblioteca/src/components/common/Searchbox.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Button from './Button';
 import { ImSearch } from 'react-icons/im';
 
-const SearchBox = () => {
+interface ISearchBoxProps{
+    placeholder: string
+}
+
+const SearchBox = ({placeholder}:ISearchBoxProps) => {
     return (
         <div className="flex items-center">
             <input
                 type="text"
-                placeholder="Pesquisar"
+                placeholder={placeholder}
                 className="px-4 py-2 flex-grow rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 mt-3 w-4/5 justify-center"
             />
             <Button className="px-3.5 py-1.5 bg-[#00ff88] rounded-md text-center uppercase tracking-wider font-extrabold mt-3 ml-3 ">

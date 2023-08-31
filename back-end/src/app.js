953be389a002ae44e37118f1a9af86b2a5b0c2b9
Thabata Moraes <thabatams16@gmail.com>
2023-08-30 22:52:32 -0300

    feat: Working on borrow crud

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,11 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import prisma from "./database/client.js";
 
 import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+
+
 
 const app = express();
 
@@ -14,5 +16,17 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+import usersRouter from "./routes/users.js";
+import bookRouter from "./routes/book.js";
+// import studentsRouter from "./routes/student.js";
+import reservesRouter from "./routes/reserve.js";
+import borrowRouter from "./routes/borrow.js";
+
+app.use("/book", bookRouter);
+// app.use("/students", studentsRouter);
+app.use("/reserves", reservesRouter);
+app.use("/borrow", borrowRouter);
+
 
 export default app;
---
+++ back-end/src/controllers/borrow.js
@@ -0,0 +1,99 @@
+import prisma from '../database/client.js'
+
+const borrow = {}
+
+borrow.create = async function(req, res){
+    const date = new Date()
+    date.setDate(date.getDate() + 14); 
+    try{
+        await prisma.operation.create({
+            data: {
+                ...req.body,
+                type: "BORROW",
+                expectedDate: date,
+            }
+        })
+        res.status(201).end()
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+borrow.retrieveAll = async function(req, res){
+    try{
+        const result = await prisma.operation.findMany({
+            where: {type: "BORROW"},
+            // orderBy: [
+            //     // {student: 'asc'},
+            //     // {date_borrow: 'asc'} 
+            //     // data sera que tbm é assim?  
+            // ]
+        })
+        res.send(result)
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+borrow.retrieveOneName = async function(req, res){
+    try{
+        const result = await prisma.borrow.findUnique({
+            where: {name_student: req.params.name_student} 
+        })
+        if(result) res.send(result)
+        else res.status(404).end()
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+borrow.retrieveOneId = async function(req, res){
+    try{
+        const result = await prisma.borrow.findUnique({
+            where: {id_borrow: req.params.id_borrow}
+        })
+        if(result) res.send(result)
+        else res.status(404).end()
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+borrow.update = async function(req, res){
+    try{
+        const result = await prisma.borrow.update({
+            where: {id_borrow: Number(req.params.id_borrow)},
+            data: req.body
+        })
+        if(result) res.status(204).end()
+        else res.status(404).end()
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+borrow.delete = async function(req, res){
+    try{
+        const result = await prisma.borrow.delete({
+            where: {id_borrow: Number(req.params.id_borrow)}
+        })
+        if(result) res.status(204).end()
+        else res.status(500).send(error)
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+export default borrow

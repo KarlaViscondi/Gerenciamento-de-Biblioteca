b6afc1643ab68967670cdcb65ae240142c33b502
Gabriel Santos <gssilva2711@gmail.com>
2023-10-19 20:54:26 -0300

    (19/10)

--- front-end/src/pages/login.tsx
+++ front-end/src/pages/login.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import myfetch from '../utils/myfetch'
 
 export default function Login() {
-
-    const [state, setState] = React.useState({
+    
+    const [state, setState] = useState({
       user: {
         email: '',
         password: ''
@@ -13,15 +13,12 @@ export default function Login() {
         user
     } = state
     
-    async function handleFormSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        event.preventDefault()        // Evita o recarregamento da página
-        setState({ ...state})
+    async function handleFormSubmit(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
         try {
-            await myfetch.post('user/login', user)
-                setState({
-                    ...state
-                })
-            }
+            await myfetch.post("http://localhost:8080/user/login")
+            console.log("Success\br", user.email + "\br" + user.password)
+        }
         catch(error) {
             setState({
                 ...state
@@ -30,4 +27,20 @@ export default function Login() {
             console.error(error)
         }
     }
+    return (
+        <>  
+            <title> Login </title>
+            <div>
+                <h1> LOGIN </h1>
+                <form onSubmit={() => handleFormSubmit}>
+                    <div>
+                        <input type="text" placeholder='Email' onChange={(e) => setState(e.target.value)} />
+                        <input type="password" placeholder='Senha'  onChange={(e) => setState(e.target.value)} />
+                        <input type="submit" value="Entrar" />
+                    </div>
+                </form>
+            </div>
+
+        </>
+    )
 }

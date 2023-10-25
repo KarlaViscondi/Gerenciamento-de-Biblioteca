93bc9da101deeb1b0b359f0ddc811c3bc0d4b9e7
KarlaViscondi <90910654+KarlaViscondi@users.noreply.github.com>
2023-10-25 18:31:27 +0000

    fix: OperationModal, OpModalContent, OpModalResults

--- front-end/src/components/common/OperationModal.tsx
+++ front-end/src/components/common/OperationModal.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { AiOutlinePlus } from 'react-icons/ai';
-import OpModalContent from '../common/OpModalContent'
+import OpModalContent from '../common/OpModalContent';
 import myfetch from '@/src/utils/myfetch';
+import search from '@/src/utils/search';
 
 interface OperationModalProps {
     closeModal: () => void;
@@ -11,46 +12,47 @@ interface OperationModalProps {
 function OperationModal({ closeModal }: OperationModalProps) {
     const [selectedOption, setSelectedOption] = useState<string>('');
     const [searchValue, setSearchValue] = useState<string>('');
-    const [result, setResult] = useState<string>('');
 
     const handleCloseModal = () => {
         closeModal();
     }
 
-    async function handleConfirm(selectedOption: string, searchValue: string, url:string) {
+    async function handleConfirm() {
+        if (!selectedOption || !searchValue) {
+            console.error('Preencha todos os campos antes de criar uma operação.');
+            return;
+        }
+
+        const url = search('operation', selectedOption, searchValue);
+
         try {
             const response = await myfetch.post(url, {
-                selectedOption, // Os dados necessários para criar a operação
+                selectedOption,
                 searchValue,
             });
-    
+
             if (response.ok) {
-                // A operação foi criada com sucesso
                 console.log('Operação criada com sucesso!');
             } else {
-                // Lida com erros na criação da operação (por exemplo, validações no servidor)
                 console.error('Erro ao criar a operação');
             }
         } catch (error) {
-            // Lida com erros na requisição (por exemplo, falha na rede)
             console.error('Erro na requisição:', error);
         }
     }
-    
 
     const handleSearch = (value: string) => {
         setSearchValue(value);
-    };
+    }
 
     const modalStyles = {
         content: {
-            width: '55%', // Largura 
-            height: '55vh', // Altura 
-            margin: 'auto', 
-            padding: '40px', 
+            width: '55%', // Largura
+            height: '55vh', // Altura
+            margin: 'auto',
+            padding: '40px',
             display: 'flex',
             flexDirection: 'column',
-            
         },
     };
 
@@ -58,7 +60,7 @@ function OperationModal({ closeModal }: OperationModalProps) {
         <div>
             <div>
                 <Modal
-                    isOpen={true} 
+                    isOpen={true}
                     onRequestClose={handleCloseModal}
                     contentLabel="Operation Modal"
                     style={modalStyles}
@@ -66,10 +68,10 @@ function OperationModal({ closeModal }: OperationModalProps) {
                     <OpModalContent
                         selectedOption={selectedOption}
                         searchValue={searchValue}
-                        result={result}
                         handleSearch={handleSearch}
                         handleSelectChange={(value: string) => setSelectedOption(value)}
-                        handleSearchClick={handleConfirm}              />
+                        handleSearchClick={handleConfirm}
+                    />
                 <div>
                     <button onClick={handleConfirm}>Confirmar</button>
                     <button onClick={handleCloseModal}>Fechar</button>
@@ -79,4 +81,5 @@ function OperationModal({ closeModal }: OperationModalProps) {
         </div>
     );
 }
+
 export default OperationModal;
